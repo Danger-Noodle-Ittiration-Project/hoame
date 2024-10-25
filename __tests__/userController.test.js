1e6357dd4cd2d96e19da2bfd4843ad7be4082fdf
PedramKashani <pedram_kashani@yahoo.com>
2024-10-24 19:53:00 -0700

    started Tdd

---
+++ __tests__/userController.test.js
@@ -0,0 +1,12 @@
+const request = require("supertest");
+const app = require("../app"); 
+const db = require("../models/hoameModels");
+
+
+// test if signup assigns pending_approval;
+
+
+// test admin can fetch users with pending_approval role 
+
+
+// admin can Approve a user by assigning a new role
