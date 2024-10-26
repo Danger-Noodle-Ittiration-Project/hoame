a23d81a8aae63b7586dd854d48290f47e16e5c66
PedramKashani <pedram_kashani@yahoo.com>
2024-10-26 10:05:59 -0700

    started TDD

--- __tests__/userController.test.js
+++ __tests__/userController.test.js
@@ -4,7 +4,11 @@ const db = require("../models/hoameModels");
 
 
 // test if signup assigns pending_approval;
-
+describe('User Signup', () => {
+    Test('should assign "Pending_approval" role when a  user signs up', asyc() => {
+        
+    })
+})
 
 // test admin can fetch users with pending_approval role 
 
