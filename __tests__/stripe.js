15882a6665415a045f41c70b8de622d810519b76
PedramKashani <pedram_kashani@yahoo.com>
2024-10-30 19:39:46 -0700

    styles roleReassigner

--- __tests__/stripe.js
+++ __tests__/stripe.js
@@ -1,7 +1,7 @@
 const request = require('supertest');
 const app = require("../server/server");
 const server = 'http://localhost:3000';
-
+const httpMocks = require("node-mocks-http");
 // const stripe = require('stripe')('sk_test_51QDFF8ANkqZlajimk7PB1U1Hu6yEUKSgsWCtYHartsMg31cfrHVDDo0Gz0Jgw0MI3yFJjuTdeXwprkFRcBVkBCfC004pjydbus');
 
 jest.mock('stripe', () => {
@@ -15,8 +15,20 @@ describe('/create-payment-intent', () => {
     
 
     const response = await request(app)
-      .post('/api/create-payment-intent')
-      .send({ amount: 10000 })
+          .post('/api/create-payment-intent', (req, res))
+          //.send({ amount: 10000 })
+
+
+      const req = httpMocks.createRequest({
+           method: "GET",
+            url: "/api/create-payment-intent",
+            headers: { Authorization: `Bearer ${response.body.tokens.access.token}` },
+            body: {amount: 10000},
+          });
+          const res = httpMocks.createResponse();
+          const next = jest.fn();
+
+          
 
     expect(200);
     expect(response.body).toEqual({ id: 100 })
